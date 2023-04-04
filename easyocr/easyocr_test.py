c0d7c66da414fe7631fb5eb4fb44f44943d002b9
fblobel <falk.blobel@spectee.com>
2023-04-04 17:13:51 +0900

    fix: add japanese writing to easyocr test

--- easyocr/easyocr_test.py
+++ easyocr/easyocr_test.py
@@ -1,20 +1,32 @@
 import easyocr
 import os
-import cv2
+from PIL import Image, ImageDraw, ImageFont
 from glob import glob
 
+
+def puttext(img, text, box, color=(255, 0, 0)):
+    size = int(min(img.size) / 20)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(
+        font=r"/System/Library/Fonts/Hiragino Sans GB.ttc", size=size
+    )
+    tx, ty, bx, by = box
+    draw.text(xy=(tx, ty - (size + 5)), text=text, fill=(255, 0, 0), font=font)
+    if tx < bx and ty < by:
+        draw.rectangle(xy=(tx, ty, bx, by), outline=color, width=int(size / 10))
+    return image
+
+
 image_paths = glob("../data/*.jpeg")
-reader = easyocr.Reader(["en"])
+reader = easyocr.Reader(["ja", "en"])
 for image_path in image_paths:
-    image = cv2.imread(image_path)
+    image = Image.open(image_path)
     result = reader.readtext(image)
-    fulltext = ""
-    for (coord, text, prob) in result:
-        (topleft, topright, bottomright, bottomleft) = coord
-        tx, ty = (int(topleft[0]), int(topleft[1]))
-        bx, by = (int(bottomright[0]), int(bottomright[1]))
-        image = cv2.rectangle(image, (tx, ty), (bx, by), (0, 0, 255), 5)
-        fulltext += text.strip()
+
     if len(result) != 0:
-        cv2.imwrite("en"+os.path.basename(image_path), image)
-        print("\t".join([fulltext, os.path.basename(image_path)]))
+        for (coord, text, prob) in result:
+            (topleft, topright, bottomright, bottomleft) = coord
+            tx, ty = (int(topleft[0]), int(topleft[1]))
+            bx, by = (int(bottomright[0]), int(bottomright[1]))
+            image = puttext(image, text, (tx, ty, bx, by))
+        image.save(os.path.basename(image_path))
